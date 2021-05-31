bb5af0f9d547375221cc11ee620af818f835e4c7
Martín Villagra <mvillagra0@gmail.com>
2021-05-31 21:40:45 +0100

    improve compativility

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 TG_TOKEN = secrets.TG_TOKEN
 CC_API_KEY = secrets.CC_API_KEY
-DB_FILENAME = Path("data") / "db.sqlite"
-PRICES_DB_FILENAME = Path("data") / "prices.sqlite"
+DB_FILENAME = str(Path("data") / "db.sqlite")
+PRICES_DB_FILENAME = str(Path("data") / "prices.sqlite")
 HELP_FILENAME = 'help.md'
