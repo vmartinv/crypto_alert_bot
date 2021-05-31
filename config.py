bfa5cc32dce2c2bd099787c3427744546a442a6d
Martín Villagra <mvillagra0@gmail.com>
2021-05-31 23:32:41 +0100

    update readme

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ TG_TOKEN = secrets.TG_TOKEN
 CC_API_KEY = secrets.CC_API_KEY
 DB_FILENAME = str(Path("data") / "db.sqlite")
 PRICES_DB_FILENAME = str(Path("data") / "prices.sqlite")
-HELP_FILENAME = 'help.md'
+HELP_FILENAME = 'readme.md'
