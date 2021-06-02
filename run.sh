b45ad959080bbcd524a54db8d9ac86ecb119d1e4
Martín Villagra <mvillagra0@gmail.com>
2021-06-02 16:15:31 +0100

    rename branch

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
-source .venv/bin/activate
-logfile="nohup.out"
-if [ -f "$logfile" ]
-then mv "$logfile" "$logfile.$(date +%F-%T)"
-fi
+#!/usr/bin/env bash
 
+source .venv/bin/activate
+pip install -r requirements.txt
 python3 tg_bot_service.py
